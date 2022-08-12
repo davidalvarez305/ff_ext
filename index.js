ce4cf776bc33a8b7a1f98341f21dddb95f657e13
david alvarez <david@southfloridaathleticclub.com>
2022-08-12 05:46:58 -0400

    where im leaving it for now

--- index.js
+++ index.js
@@ -1,47 +1,88 @@
 const nodes = document.body;
 
+function matchesField(fieldName, key) {
+  return fieldName.toLowerCase().includes(key);
+}
+
+function customFields(node) {
+  if (node.id.includes("job_application_answers_attributes_0_text_value")) {
+    node.value = "https://linkedin.com/";
+  }
+  if (node.id.includes("job_application_answers_attributes_1_text_value")) {
+    node.value = "https://github.com/";
+  }
+  if (node.id.includes("job_application_answers_attributes_2_text_value")) {
+    node.value = "LinkedIn";
+  }
+}
+
+function selectFields(node) {
+  console.log(node);
+  node.value = "Florida Atlantic University";
+}
+
 function enterInput(node) {
   const fields = ["id", "name", "autocomplete"];
 
   for (let i = 0; i < fields.length; i++) {
     const field = fields[i];
-    console.log(field);
     if (node[field]) {
       switch (true) {
-        case node[field].includes("first") && node[field].includes("name"):
+        case matchesField(node[field], "first") &&
+          matchesField(node[field], "name"):
           node.value = "First";
           break;
-        case node[field].includes("email"):
+        case matchesField(node[field], "email"):
           node.value = "email@gmail.com";
           break;
-        case node[field].includes("last") && node[field].includes("name"):
+        case matchesField(node[field], "password"):
+          node.value = "password";
+          break;
+        case matchesField(node[field], "last") &&
+          matchesField(node[field], "name"):
           node.value = "Name";
           break;
-        case node[field].includes("full") && node[field].includes("name"):
+        case matchesField(node[field], "full") &&
+          matchesField(node[field], "name"):
           node.value = "First Name";
           break;
-        case node[field].includes("name") && !node[field].includes("middle"):
+        case matchesField(node[field], "name") &&
+          !matchesField(node[field], "middle"):
           node.value = "First Name";
           break;
-        case node[field].includes("country"):
+        case matchesField(node[field], "country"):
           node.value = "My Country";
           break;
-        case node[field].includes("city"):
+        case matchesField(node[field], "city"):
           node.value = "My City";
           break;
-        case node[field].includes("zip") || node[field].includes("postal"):
+        case matchesField(node[field], "zip") ||
+          matchesField(node[field], "postal"):
           node.value = 12345;
           break;
-        case node[field].includes("phone") || node[field].includes("tel"):
+        case matchesField(node[field], "phone") ||
+          matchesField(node[field], "tel"):
           node.value = 1234567890;
           break;
-        case node[field].includes("address"):
+        case matchesField(node[field], "address"):
           node.value = "1234 Some House Drive";
           break;
-        case node[field].includes("about"):
+        case matchesField(node[field], "about"):
           node.value = "This is the about me section.";
           break;
+        case matchesField(node[field], "linkedin"):
+          node.value = "https://www.linkedin.com/";
+          break;
+        case matchesField(node[field], "additional"):
+          node.value = "Additional information field.";
+          break;
+        case matchesField(node[field], "select2-drop-mask"):
+          selectFields(node);
+          break;
         default:
+          if (field === "id") {
+            customFields(node);
+          }
           break;
       }
     }
