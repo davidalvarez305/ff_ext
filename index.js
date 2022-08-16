2508c751a860a2af954768271b872b1ec8424753
david alvarez <david@southfloridaathleticclub.com>
2022-08-16 01:35:30 -0400

    made some breakthrough but still need to figure out select values

--- index.js
+++ index.js
@@ -1,36 +1,94 @@
 const nodes = document.body;
 
 function matchesField(fieldName, key) {
-  return fieldName.toLowerCase().includes(key);
+  return fieldName.toLowerCase().includes(key.toLowerCase());
 }
 
 function customFields(node) {
-  if (node.id.includes("job_application_answers_attributes_0_text_value")) {
-    node.value = "https://linkedin.com/";
-  }
-  if (node.id.includes("job_application_answers_attributes_1_text_value")) {
-    node.value = "https://github.com/";
-  }
-  if (node.id.includes("job_application_answers_attributes_2_text_value")) {
-    node.value = "LinkedIn";
+  if (node["labels"] && node["labels"][0]) {
+    const label = node["labels"][0].innerText;
+    console.log(label);
+    switch (true) {
+      case matchesField(label, "linkedin profile"):
+        node.value = "LinkedIn";
+        break;
+      case matchesField(label, "github"):
+        node.value = "Github";
+        break;
+      case matchesField(label, "salary") || matchesField(label, "compensation"):
+        node.value = "Open";
+        break;
+      case matchesField(label, "how did you hear about this job"):
+        node.value = "LinkedIn";
+        break;
+      case matchesField(label, "state"):
+        node.value = "Florida";
+        break;
+      case matchesField(label, "gender"):
+        node.value = "Male";
+        break;
+      case matchesField(label, "hispanic"):
+        node.value = "Yes";
+        break;
+      case matchesField(label, "veteran"):
+        node.value = "I am not a protected veteran";
+        break;
+      case matchesField(label, "authorized to work in the united states"):
+        node.value = "Yes";
+        break;
+      default:
+        node.value = "";
+    }
   }
 }
 
-function selectFields(node) {
-  console.log(node);
-  node.value = "Florida Atlantic University";
+function resolveName(node, field) {
+  let name = "";
+  let fieldName = "";
+
+  if (!node["labels"]) {
+    fieldName = node[field];
+  } else {
+    fieldName = node["labels"][0].innerText;
+  }
+
+  switch (true) {
+    case matchesField(fieldName, "first"):
+      name = "First Name";
+      break;
+    case matchesField(fieldName, "last"):
+      name = "Last Name";
+      break;
+    case matchesField(fieldName, "full"):
+      name = "Full Name";
+      break;
+    case matchesField(fieldName, "middle"):
+      name = "Middle Name";
+      break;
+    default:
+      name = "Full Name";
+  }
+
+  return name;
+}
+
+function findSiblings(node) {
+  node.nextSibling;
 }
 
 function enterInput(node) {
-  const fields = ["id", "name", "autocomplete"];
+  const fields = ["id", "name", "autocomplete", "className"];
+
+  if (node["nodeName"] === "SELECT") {
+    console.log("Select node: ", node);
+  }
 
   for (let i = 0; i < fields.length; i++) {
     const field = fields[i];
     if (node[field]) {
       switch (true) {
-        case matchesField(node[field], "first") &&
-          matchesField(node[field], "name"):
-          node.value = "First";
+        case matchesField(node[field], "name"):
+          node.value = resolveName(node, field);
           break;
         case matchesField(node[field], "email"):
           node.value = "email@gmail.com";
@@ -38,20 +96,8 @@ function enterInput(node) {
         case matchesField(node[field], "password"):
           node.value = "password";
           break;
-        case matchesField(node[field], "last") &&
-          matchesField(node[field], "name"):
-          node.value = "Name";
-          break;
-        case matchesField(node[field], "full") &&
-          matchesField(node[field], "name"):
-          node.value = "First Name";
-          break;
-        case matchesField(node[field], "name") &&
-          !matchesField(node[field], "middle"):
-          node.value = "First Name";
-          break;
         case matchesField(node[field], "country"):
-          node.value = "My Country";
+          node.value = "United States";
           break;
         case matchesField(node[field], "city"):
           node.value = "My City";
@@ -76,13 +122,8 @@ function enterInput(node) {
         case matchesField(node[field], "additional"):
           node.value = "Additional information field.";
           break;
-        case matchesField(node[field], "select2-drop-mask"):
-          selectFields(node);
-          break;
         default:
-          if (field === "id") {
-            customFields(node);
-          }
+          customFields(node);
           break;
       }
     }
