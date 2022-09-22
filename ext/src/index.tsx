e3eb9f6e5888822af541960f5d43bb9f41b39028
david alvarez <david@southfloridaathleticclub.com>
2022-09-22 06:53:55 -0400

    flask

--- ext/src/index.tsx
+++ ext/src/index.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import { App } from "./App";
 
-const container = document.getElementById("root");
+const container = document.getElementById("extension");
 if (!container) throw new Error("Failed to find the root element");
 const root = ReactDOM.createRoot(container);
 
