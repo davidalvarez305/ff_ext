47268aae0fa8712eddf108c49346d6d663e90f41
david alvarez <david@southfloridaathleticclub.com>
2022-10-01 23:18:17 -0400

    done with bamboo

--- server/utils.py
+++ server/utils.py
@@ -247,11 +247,10 @@ def handle_textarea(element, user_data):
     if textarea.get_attribute('value') == "":
         textarea.send_keys(user_data)
 
-def handle_select_div(element, user_data):
-    options = element.find_elements(By.CLASS_NAME, "fab-MenuOption__row")
+def handle_select_div(driver, user_data):
+    options = driver.find_elements(By.CLASS_NAME, "fab-MenuOption__row")
     for option in options:
         option_name = option.get_attribute('innerText')
-        print('option name: ', option_name)
         if user_data.lower() in option_name.lower():
             option.click()
 
@@ -264,19 +263,22 @@ def handle_bamboo(driver, data):
 
             # Handle Radiobuttons
             if "Veteran" in field_name:
-                btns = driver.find_elements(By.CLASS_NAME, "fab-Radio")
+                btns = driver.find_elements(By.XPATH, '//*[@type="radio"]')
                 for btn in btns:
-                    btn_text = btn.get_attribute('innerText')
-                    if data['user']['veteranStatus'].lower() in btn_text.lower():
+                    label = btn.find_element(By.XPATH, '../label').get_attribute('innerText')
+                    if data['user']['veteranStatus'].lower() in label.lower():
                         btn.click()
 
             # Handle Selects
             if "Gender" in field_name:
-                handle_select_div(element, data['user']['gender'])
+                element.click()
+                handle_select_div(driver, data['user']['gender'])
             elif "Disability" in field_name:
-                handle_select_div(element, data['user']['disabilityStatus'])
+                element.click()
+                handle_select_div(driver, data['user']['disabilityStatus'])
             elif "Ethnicity" in field_name:
-                handle_select_div(element, data['user']['race'])
+                element.click()
+                handle_select_div(driver, data['user']['race'])
 
             # Handle Inputs
             else:
@@ -286,4 +288,5 @@ def handle_bamboo(driver, data):
                         handle_textarea(element, data['user'][f"{field}"])
 
         except BaseException as err:
+            print(err)
             continue
